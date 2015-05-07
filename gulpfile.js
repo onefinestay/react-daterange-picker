18968f9fc7161b3c9aad3e950cc369d72fffb442
Jonathan Kim <jkimbo@gmail.com>
2015-05-07 11:15:18 +0100

    Just use a straight react compile instead of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 
 Object.assign = require('object.assign');
 
+var fs = require('fs');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var autoprefixer = require('gulp-autoprefixer');
 var extReplace = require('gulp-ext-replace');
 var watch = require('gulp-watch');
@@ -11,55 +11,7 @@ var babel = require('gulp-babel');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var deploy = require('gulp-gh-pages');
-var through = require('through2');
 var React = require('react');
-var Module = module.constructor;
-
-function staticRender() {
-  // setup babel hook
-  require("babel/register");
-
-  // Creates a stream through which each file will pass
-  var stream = through.obj(function(file, enc, cb) {
-    if (!file.isNull()) {
-      if (file.isStream()) {
-        this.emit('error', new gutil.PluginError('static-render', 'Streams are not supported!'));
-        return cb();
-      }
-
-      if (file.isBuffer()) {
-        try {
-          var contents = file.contents.toString('utf8');
-
-          var m = new Module();
-          m.id = file.path;
-          m.filename = file.path;
-          m.paths = module.paths;
-          m._compile(contents, file.path);
-          var Component = React.createFactory(m.exports);
-          var markup = React.renderToString(Component());
-
-          markup = '<!document html>' + markup;
-
-          file.contents = new Buffer(markup);
-          var filename = gutil.replaceExtension(file.path, '.html');
-          file.path = filename;
-        } catch (err) {
-          this.emit('error', new gutil.PluginError('static-render', err));
-          return cb();
-        }
-      }
-    }
-
-    // Make sure the file goes through the next gulp plugin
-    this.push(file);
-    // Tell the stream engine that we are done with this file
-    return cb();
-  });
-
-  // Return the file stream
-  return stream;
-}
 
 gulp.task('build-js', function() {
   // build javascript files
@@ -80,13 +32,17 @@ gulp.task('watch-js', function() {
 });
 
 gulp.task('build-example', ['build-js'], function() {
-  return gulp.src('./example/index.jsx')
-    .pipe(babel({
-      stage: 1,
-      plugins: ['object-assign']
-    }))
-    .pipe(staticRender())
-    .pipe(gulp.dest('./example'));
+  // setup babel hook
+  require("babel/register")({
+    stage: 1,
+    plugins: ['object-assign']
+  });
+
+  var Index = React.createFactory(require('./example/index.jsx'));
+  var markup = '<!document html>' + React.renderToString(Index());
+
+  // write file
+  fs.writeFileSync('./example/index.html', markup);
 });
 
 gulp.task('build-example-scss', function() {
