652b5351a3a9aa7bee56f3fed936987982ff2610
Nicolas <nicolas.senechal@gmail.com>
2015-08-13 17:50:57 +0100

    Added tests for CalendarHighlight

--- src/calendar/tests/CalendarHighlight.spec.js
+++ src/calendar/tests/CalendarHighlight.spec.js
@@ -1,17 +1,17 @@
 import React from 'react/addons';
-import CalendarSelection from '../CalendarSelection.jsx';
+import CalendarHighlight from '../CalendarHighlight.jsx';
 
 import BemMixin from '../../utils/BemMixin';
 
 
 const TestUtils = React.addons.TestUtils;
 
-describe('The CalendarSelection Component', function () {
+describe('The CalendarHighlight Component', function () {
     beforeEach(() => {
-        this.spyCx = spyOn(CalendarSelection.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
+        this.spyCx = spyOn(CalendarHighlight.prototype.__reactAutoBindMap, 'cx').and.returnValue('my-class');
 
         var shallowRenderer = TestUtils.createRenderer();
-        shallowRenderer.render(<CalendarSelection pending={true} modifier='test'/>);
+        shallowRenderer.render(<CalendarHighlight pending={true} modifier='test'/>);
         this.renderedComponent = shallowRenderer.getRenderOutput();
     });
 
@@ -19,7 +19,6 @@ describe('The CalendarSelection Component', function () {
         expect(this.renderedComponent.type).toBe('div');
         expect(this.spyCx).toHaveBeenCalledWith({
             states: {
-                pending: true
             },
             modifiers: {
                 test: true
