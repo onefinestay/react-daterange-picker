7b6a5178970b2ecafcef68d6dd1309bfa7feb658
Taylor Baldwin <taylorbaldwin@gmail.com>
2015-10-21 18:32:48 -0400

    built files

--- dist/calendar/CalendarHighlight.js
+++ dist/calendar/CalendarHighlight.js
@@ -8,9 +8,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'd
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var _reactAddons = require('react/addons');
+var _react = require('react');
 
-var _reactAddons2 = _interopRequireDefault(_reactAddons);
+var _react2 = _interopRequireDefault(_react);
 
 var _utilsBemMixin = require('../utils/BemMixin');
 
@@ -20,13 +20,13 @@ var _utilsPureRenderMixin = require('../utils/PureRenderMixin');
 
 var _utilsPureRenderMixin2 = _interopRequireDefault(_utilsPureRenderMixin);
 
-var CalendarHighlight = _reactAddons2['default'].createClass({
+var CalendarHighlight = _react2['default'].createClass({
   displayName: 'CalendarHighlight',
 
   mixins: [_utilsBemMixin2['default'], _utilsPureRenderMixin2['default']],
 
   propTypes: {
-    modifier: _reactAddons2['default'].PropTypes.string
+    modifier: _react2['default'].PropTypes.string
   },
 
   render: function render() {
@@ -35,7 +35,7 @@ var CalendarHighlight = _reactAddons2['default'].createClass({
     var modifiers = _defineProperty({}, modifier, true);
     var states = {};
 
-    return _reactAddons2['default'].createElement('div', { className: this.cx({ states: states, modifiers: modifiers }) });
+    return _react2['default'].createElement('div', { className: this.cx({ states: states, modifiers: modifiers }) });
   }
 });
 
